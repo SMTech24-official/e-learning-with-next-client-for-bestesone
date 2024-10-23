aa6133da98db81d4cf7cff7746a5542db431ae93
Kawser Simanto <kawsersimanto@gmail.com>
2024-10-23 14:45:09 +0600

    initialized banner

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -11,18 +12,29 @@ const config: Config = {
       colors: {
         primary: "#7F56D9",
         "primary-light": "#F7F3FF",
+        "gray-neutral-900": "#111927",
+        "gray-neutral-500": "#667085",
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
       container: {
         screens: {
-          DEFAULT: "1200px",
+          DEFAULT: "1290px",
         },
         center: true,
-        padding: "2rem",
+        padding: "1.2rem",
+      },
+      backgroundImage: {
+        "black-gradient":
+          "linear-gradient(178deg, rgba(0, 0, 0, 0.00) 44.14%, #000 98.09%)",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
