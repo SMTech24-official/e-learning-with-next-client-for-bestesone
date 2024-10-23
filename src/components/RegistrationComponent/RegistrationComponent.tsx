19faa7a74e381e6996d0686dea46d474734722e4
Sadik Ahmmed Tonmoy <Sadikahmed2258@gmail.com>
2024-10-23 14:49:54 +0600

    registration page update

--- src/components/RegistrationComponent/RegistrationComponent.tsx
+++ src/components/RegistrationComponent/RegistrationComponent.tsx
@@ -2,13 +2,14 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Button, Divider } from "antd";
 import Image from "next/image";
-import React from "react";
 import { z } from "zod";
-import MyFormWrapper from "../ui/MyForm/MyFormWrapper/MyFormWrapper";
 import MyFormInput from "../ui/MyForm/MyFormInput/MyFormInput";
 import MyFormSelect from "../ui/MyForm/MyFormSelect/MyFormSelect";
-import { Select } from "antd";
+import MyFormWrapper from "../ui/MyForm/MyFormWrapper/MyFormWrapper";
+import { FcGoogle } from "react-icons/fc";
+import { FaApple, FaFacebook } from "react-icons/fa";
 
 const validationSchema = z.object({
   full_name: z
@@ -70,97 +71,109 @@ const RegistrationComponent = () => {
     console.log(data);
   };
 
-  const InterestOptions = [
-    { value: "Math", label: "Math" },
-    { value: "Science", label: "Science" },
-    { value: "English", label: "English" },
-    { value: "Social Studies", label: "Social Studies" },
-    { value: "Computer Science", label: "Computer Science" },
-    { value: "Arts", label: "Arts" },
-    { value: "Music", label: "Music" },
-    { value: "Language", label: "Language" },
-    { value: "Health", label: "Health" },
-    { value: "Business", label: "Business" },
-    { value: "Other", label: "Other" },
-  ];
   return (
     <>
-      <div className="h-full flex justify-center items-center">
-        <div className="w-full flex flex-col items-center justify-center">
-          <Image
-            className="w-36 mb-6 text-center"
-            src={"https://i.ibb.co.com/s5vxFJb/image-32.png"}
-            height={500}
-            width={500}
-            alt="Logo"
+      <div className="w-full flex flex-col items-center justify-center mt-8 px-2">
+        <Image
+          className="w-36 mb-6 text-center"
+          src={"https://i.ibb.co.com/s5vxFJb/image-32.png"}
+          height={500}
+          width={500}
+          alt="Logo"
+        />
+        <h3 className="text-center text-xl font-semibold leading-[30px] mb-6">
+          Register
+        </h3>
+
+        <MyFormWrapper
+          className={"flex flex-col gap-5 w-full md:max-w-xl "}
+          onSubmit={handleSubmit}
+          resolver={zodResolver(validationSchema)}
+        >
+          <MyFormInput
+            name={"full_name"}
+            label={"Full name"}
+            type={"text"}
+            placeHolder="Enter your name"
           />
-          <h3 className="text-center text-xl font-semibold leading-[30px] mb-6">
-            Register
-          </h3>
+          <MyFormInput
+            name={"phone"}
+            label={"Phone Number"}
+            type={"number"}
+            placeHolder="Enter your phone number"
+          />
+          <MyFormInput
+            name={"email"}
+            label={"Email"}
+            type={"email"}
+            placeHolder="Enter your email"
+          />
+          <MyFormInput
+            name={"address"}
+            label={"Address"}
+            placeHolder="Enter your address"
+          />
+          <MyFormInput
+            name={"age"}
+            label={"Age (optional)"}
+            type={"number"}
+            placeHolder="Enter your age"
+          />
+          <MyFormSelect
+            name={"Interested"}
+            label={"Interested (optional)"}
+            options={[
+              { value: "1", label: "Jack" },
+              { value: "2", label: "Lucy" },
+              { value: "3", label: "Tom" },
+            ]}
+            placeHolder="Select your interested subject"
+          />
+          <MyFormInput
+            type="password"
+            name={"password"}
+            label={"Password"}
+            placeHolder="Enter your password"
+          />
 
-          <MyFormWrapper
-            className={"flex flex-col gap-5 w-full md:max-w-3xl px-2"}
-            onSubmit={handleSubmit}
-            resolver={zodResolver(validationSchema)}
-          >
-            <MyFormInput
-              name={"full_name"}
-              label={"Full name"}
-              type={"text"}
-              placeHolder="Enter your name"
-            />
-            <MyFormInput
-              name={"phone"}
-              label={"Phone Number"}
-              type={"number"}
-              placeHolder="Enter your phone number"
-            />
-            <MyFormInput
-              name={"email"}
-              label={"Email"}
-              type={"email"}
-              placeHolder="Enter your email"
-            />
-            <MyFormInput
-              name={"address"}
-              label={"Address"}
-              placeHolder="Enter your address"
-            />
-            <MyFormInput
-              name={"age"}
-              label={"Age (optional)"}
-              type={"number"}
-              placeHolder="Enter your age"
-            />
-            <MyFormSelect
-              name={"Interested"}
-              label={"Interested (optional)"}
-              options={[
-                { value: "1", label: "Jack" },
-                { value: "2", label: "Lucy" },
-                { value: "3", label: "Tom" },
-              ]}
-              placeHolder="Select your interested subject"
-            />
-            <MyFormInput
-              type="password"
-              name={"password"}
-              label={"Password"}
-              placeHolder="Enter your password"
-            />
-
-            {/* <MyFormPasswordInput name={"password"} label={"password"} type={"password"} onValueChange={setPassword}/>
+          {/* <MyFormPasswordInput name={"password"} label={"password"} type={"password"} onValueChange={setPassword}/>
         <MyFormPasswordInput name={"c_password"} label={"c_password"} type={"password"} matchWith={"password"} onValueChange={setConfirmPassword}/>
         <MyFormSelect name={"select"} label={"select"} options={option}/>
         <MyFormSelectWithWatch name={"select - 2"} options={option} label={"select"} onValueChange={setCourseId} />
         <MyFormTimePicker name={"timepicker"} label={"Time Picker"}  /> */}
-            <button
-              className="border w-min py-2 px-3 rounded bg-blue-400 text-white mt-2"
-              type="submit"
-            >
-              Submit
-            </button>
-          </MyFormWrapper>
+          <Button
+            className=" w-full py-5 rounded-lg bg-primary text-white text-base font-normal leading-6"
+            htmlType="submit"
+          >
+            Get Started
+          </Button>
+        </MyFormWrapper>
+
+        <div className=" mb-10">
+          <p className="text-base leading-6 text-[#ABABAB] font-normal my-8">
+            If you already have any account please{" "}
+            <span className="text-[#FF8C00] font-medium">log in</span>
+          </p>
+
+          <Divider style={{ borderColor: "#7F56D9" }} className="">
+            <span className="text-[#667085] text-base leading-6 font-normal">
+              Or sign up with
+            </span>
+          </Divider>
+          <div className="flex items-center justify-center gap-4 my-8">
+            <Button className="text-base font-medium leading-6 sm:px-4 py-[18px] rounded-lg ">
+              <FcGoogle size={22} />
+              <p className="hidden sm:block "> Google</p>
+            </Button>
+            <Button className="text-base font-medium leading-6 sm:px-4  py-[18px] rounded-lg ">
+              <FaApple size={22} />
+              <p className="hidden sm:block "> Apple</p>
+            </Button>
+            <Button className="text-base font-medium leading-6 sm:px-4 py-[18px] rounded-lg ">
+              <FaFacebook size={22} className="text-blue-500" />
+              <p className="hidden sm:block"> Facebook</p>
+            </Button>
+          </div>
         </div>
       </div>
     </>
