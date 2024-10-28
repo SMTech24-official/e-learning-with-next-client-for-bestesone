05e3b480654529e057f68ca952bca36aa7b17833
Kawser Simanto <kawsersimanto@gmail.com>
2024-10-28 16:54:40 +0600

    updated code

--- src/app/(withCommonLayout)/registration/page.tsx
+++ src/app/(withCommonLayout)/registration/page.tsx
@@ -1,12 +1,12 @@
-import RegistrationComponent from '@/components/RegistrationComponent/RegistrationComponent';
-import React from 'react';
+import RegistrationComponent from "@/components/RegistrationComponent/RegistrationComponent";
+import React from "react";
 
 const registrationPage = () => {
-    return (
-        <div className='h-screen '>
-           <RegistrationComponent/>
-        </div>
-    );
+  return (
+    <div className="h-screen ">
+      <RegistrationComponent />
+    </div>
+  );
 };
 
 export default registrationPage;
