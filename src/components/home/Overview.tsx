836a548ebff63e6769be7062db1a5bc8bc9137cc
Meraj Faizan <mdmaraj1500@gmail.com>
2024-12-08 09:12:52 +0600

    overview responsiveness updated

--- src/components/home/Overview.tsx
+++ src/components/home/Overview.tsx
@@ -4,20 +4,20 @@ const Overview = () => {
       <div className="container">
         <div className=" text-white py-10 md:h-64 flex flex-col gap-20 sm:flex-row md:justify-around items-center">
           <div className="flex flex-col items-center">
-            <p className="text-4xl sm:text-xl font-bold mb-2 md:items-start">100,000+</p>
-            <p className="text-xl sm:text-sm font-extralight">students worldwide</p>
+            <p className="text-4xl sm:text-xl lg:text-4xl font-bold mb-2 md:items-start">100,000+</p>
+            <p className="text-xl sm:text-sm lg:text-xl font-extralight">students worldwide</p>
           </div>
           <div className="flex flex-col items-center md:items-start">
-            <p className="text-4xl sm:text-xl font-bold mb-2">200,000+</p>
-            <p className="text-xl sm:text-sm font-extralight">Total Courses Views</p>
+            <p className="text-4xl sm:text-xl lg:text-4xl font-bold mb-2">200,000+</p>
+            <p className="text-xl sm:text-sm lg:text-xl font-extralight">Total Courses Views</p>
           </div>
           <div className="flex flex-col items-center md:items-start">
-            <p className="text-4xl sm:text-xl font-bold mb-2">5,000+</p>
-            <p className="text-xl sm:text-sm font-extralight">Five-star courses Review</p>
+            <p className="text-4xl sm:text-xl lg:text-4xl font-bold mb-2">5,000+</p>
+            <p className="text-xl sm:text-sm lg:text-xl font-extralight">Five-star courses Review</p>
           </div>
           <div className="flex flex-col items-center md:items-start">
-            <p className="text-4xl sm:text-xl font-bold mb-2">5,000+</p>
-            <p className="text-xl sm:text-sm font-extralight">Student Community</p>
+            <p className="text-4xl sm:text-xl lg:text-4xl font-bold mb-2">5,000+</p>
+            <p className="text-xl sm:text-sm lg:text-xl font-extralight">Student Community</p>
           </div>
         </div>
       </div>
