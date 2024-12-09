ada6a59981f27d3944df962b6793adbe79bc239b
Meraj Faizan <mdmaraj1500@gmail.com>
2024-12-09 17:29:27 +0600

    terms and condition page added

---
+++ src/app/(withCommonLayout)/terms-and-condition/page.tsx
@@ -0,0 +1,129 @@
+import React from "react";
+
+const TermsAndcondition = () => {
+  return (
+    <div className="container">
+      <h1 className="text-heading-text text-5xl mt-10">Terms And Condition</h1>
+      <p className="py-6 text-[#475467]">
+        Welcome to Learning With Us, a platform designed to provide access to a
+        wide variety of courses for learners of all backgrounds and interests.
+        By using this platform, you agree to abide by the terms outlined in this
+        document, as well as the accompanying Privacy Policy, Refund Policy, and
+        other policies referenced herein. These Terms and Conditions define your
+        rights, responsibilities, and obligations while using the platform and
+        form a legally binding agreement between you and Learning With Us.
+        Please read these terms carefully, as they contain important information
+        regarding your use of our services. If you do not agree to these terms,
+        you must discontinue your use of the platform immediately.
+      </p>
+
+      <h2 className="text-[#475467] text-2xl">Acceptance of Terms</h2>
+      <p className="py-6 text-[#475467]">
+        Acceptance of Terms Welcome to Learning With Us, a platform designed to
+        provide access to a wide variety of courses for learners of all
+        backgrounds and interests. By using this platform, you agree to abide by
+        the terms outlined in this document, as well as the accompanying Privacy
+        Policy, Refund Policy, and other policies referenced herein. These Terms
+        and Conditions define your rights, responsibilities, and obligations
+        while using the platform and form a legally binding agreement between
+        you and Learning With Us. Please read these terms carefully, as they
+        contain important information regarding your use of our services. If you
+        do not agree to these terms, you must discontinue your use of the
+        platform immediately. Acceptance of Terms When you access or use the
+        services offered by Learning With Us, you explicitly confirm your
+        acceptance of these Terms and Conditions in their entirety. This
+        acceptance is a requirement for accessing any features, content, or
+        services on the platform. These terms apply to all users, including
+        learners who enroll in courses, instructors who create content, and
+        anyone who otherwise interacts with the platform. By agreeing, you
+        acknowledge that you have reviewed and accepted all applicable terms,
+        including those referenced in the Privacy Policy and Refund Policy. If
+        you disagree with any part of these terms or are unable to comply, you
+        may not use any portion of the platform. Learning With Us retains the
+        right to suspend or terminate your access to the platform if you violate
+        these terms or fail to meet any of the specified conditions.
+      </p>
+      <h2 className="text-[#475467] text-2xl">Services Provided</h2>
+      <p className="py-6 text-[#475467]">
+        Learning With Us is an online platform that connects learners with
+        educational courses provided by various instructors and content
+        providers. The platform allows users to browse through available
+        courses, access course materials, and enroll in specific programs based
+        on their needs and preferences. These courses may cover a wide range of
+        topics, including academic, professional, and personal development
+        subjects. Some services on the platform are offered free of charge,
+        while others may require a subscription or one-time payment. Detailed
+        information about course pricing, access requirements, and associated
+        conditions will be made available at the time of enrollment. Users are
+        encouraged to carefully review the terms of each course before
+        proceeding with registration or payment. Learning With Us is committed
+        to providing accurate and reliable information about the courses offered
+        on the platform. However, the availability of courses, the quality of
+        the content, and the qualifications of instructors are subject to change
+        and may vary. The platform does not guarantee specific outcomes or
+        results from participating in any course, as success depends on the
+        individual effort and commitment of the user.
+      </p>
+      <h2 className="text-[#475467] text-2xl">
+        Eligibility and Age Requirements
+      </h2>
+      <p className="py-6 text-[#475467]">
+        To access and use Learning With Us, you must meet specific eligibility
+        and age requirements. These requirements ensure compliance with
+        applicable laws and provide a safe and secure environment for all users.
+        If you are a resident of Bangladesh or any country outside the USA, UK,
+        or EU, you must be at least 18 years of age to register and use the
+        platform. If you are a resident of the European Union (EU), you must be
+        at least 16 years of age to access the platform. If you reside in the
+        United States or the United Kingdom, the minimum age requirement is 13
+        years. If you are below the required age for your country of residence,
+        you may only use the platform with the explicit consent and supervision
+        of a parent or legal guardian. The guardian must read and accept these
+        Terms and Conditions on your behalf. In the event of any dispute between
+        the platform and a minor, the terms outlined in this agreement will be
+        enforceable against the parent or guardian who provided consent.
+        Learning With Us reserves the right to terminate accounts or restrict
+        access if it is discovered that users do not meet the specified age
+        requirements or if proper consent has not been obtained. Users are
+        solely responsible for ensuring compliance with these requirements
+        before creating an account or accessing the platform.
+      </p>
+      <h2 className="text-[#475467] text-2xl">Account Registration</h2>
+      <p className="py-6 text-[#475467]">
+        To fully utilize the services offered by Learning With Us, users are
+        required to register and create a personal account. The registration
+        process involves providing accurate, up-to-date information, including
+        your name, email address, and any other details required by the
+        platform. By registering, you agree to take full responsibility for
+        maintaining the confidentiality of your login credentials and ensuring
+        that your account is used in compliance with these Terms and Conditions.
+        You may not impersonate another individual, use false information, or
+        register multiple accounts for fraudulent purposes. Any such activity
+        may result in the suspension or termination of your account without
+        prior notice. Additionally, you must notify Learning With Us immediately
+        if you suspect unauthorized access to your account or identify any
+        security breaches. Certain services or features on the platform may
+        require payment or additional verification during the registration
+        process. Users are encouraged to review the terms and conditions
+        associated with each course or service before proceeding. Refunds, if
+        applicable, will be provided in accordance with the platform’s Refund
+        Policy.
+      </p>
+      <h2 className="text-[#475467] text-2xl">
+        Updates to Terms and Conditions
+      </h2>
+      <p className="py-6 text-[#475467]">
+        Learning With Us reserves the right to update or modify these Terms and
+        Conditions at any time. Updates will be communicated through the
+        platform, and the revised terms will take effect immediately upon
+        publication. By continuing to use the platform after such updates, you
+        agree to comply with the revised terms. It is your responsibility to
+        review the Terms and Conditions periodically to stay informed about any
+        changes. If you have any questions or concerns about these Terms and
+        Conditions, please contact our support team for further clarification.
+      </p>
+    </div>
+  );
+};
+
+export default TermsAndcondition;
